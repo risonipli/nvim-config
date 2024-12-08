b178d1b1b2622f8bc1377191dcd66d2ad5e492cf
Artem Prokhorov <a.prokhorov@criteo.com>
2024-12-08 20:18:11 +0100

    ignore .DS_Store files

--- lua/config/telescope.lua
+++ lua/config/telescope.lua
@@ -1,15 +1,15 @@
-require('telescope').setup({
+require("telescope").setup({
   defaults = {
     -- configure to use ripgrep
     vimgrep_arguments = {
       "rg",
-      "--follow",        -- Follow symbolic links
-      "--hidden",        -- Search for hidden files
-      "--no-heading",    -- Don't group matches by each file
+      "--follow", -- Follow symbolic links
+      "--hidden", -- Search for hidden files
+      "--no-heading", -- Don't group matches by each file
       "--with-filename", -- Print the file path with the matched lines
-      "--line-number",   -- Show line numbers
-      "--column",        -- Show column numbers
-      "--smart-case",    -- Smart case search
+      "--line-number", -- Show line numbers
+      "--column", -- Show column numbers
+      "--smart-case", -- Smart case search
 
       -- Exclude some patterns from search
       "--glob=!**/.git/*",
@@ -19,10 +19,10 @@ require('telescope').setup({
       "--glob=!**/dist/*",
       "--glob=!**/yarn.lock",
       "--glob=!**/package-lock.json",
+      "--glob=!**/.DS_Store",
     },
 
-    ...
- 
+    ...,
   },
   pickers = {
     find_files = {
@@ -38,14 +38,14 @@ require('telescope').setup({
         "--glob=!**/dist/*",
         "--glob=!**/yarn.lock",
         "--glob=!**/package-lock.json",
+        "--glob=!**/.DS_Store",
       },
     },
   },
 })
 
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
-vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
-
+local builtin = require("telescope.builtin")
+vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
+vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
+vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
+vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
